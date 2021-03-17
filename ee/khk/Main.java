db3701c70f560a3833abbb552d7be03d9cfbb7b1
Martin K <martin.kutt@khk.ee>
2021-03-17 09:58:40 +0200

    a

---
+++ ee/khk/Main.java
@@ -0,0 +1,37 @@
+package ee.khk;
+
+import com.sun.prism.Material;
+import javafx.application.Application;
+import javafx.scene.shape.Box;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.text.Text;
+
+public class Main extends Application {
+    public static void main(String[] args){
+        Application.launch(args);
+    }
+
+    public void start(Stage stage){
+        Text text = new Text("Tere tulemast põrgusse!");
+        Box box = new Box();
+        box.setWidth(50.0);
+        box.setHeight(50.0);
+        box.setLayoutY(100);
+        box.setLayoutX(100);
+        text.setLayoutY(100);
+        text.setLayoutX(100);
+
+        Group group = new Group(text,box);
+
+        Scene scene = new Scene(group);
+
+        stage.setScene(scene);
+        stage.setTitle("Morning Anouncement");
+        stage.setWidth(320);
+        stage.setHeight(320);
+        stage.show();
+    }
+
+}
