914359c78a9298e026f110a25ae18598b8873b08
Martin K <martin.kutt@khk.ee>
2021-03-23 15:32:40 +0200

    Application klass ja rakenduse elutsükkel

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -11,9 +11,11 @@ import javafx.scene.text.Text;
 public class Main extends Application {
     public static void main(String[] args){
         Application.launch(args);
+        System.out.println("Appication run");
     }
 
     public void start(Stage stage){
+        System.out.println("Application starts");
         Text text = new Text("Tere tulemast põrgusse!");
         Box box = new Box();
         box.setWidth(50.0);
@@ -34,4 +36,11 @@ public class Main extends Application {
         stage.show();
     }
 
+    public void init() throws Exception {
+        System.out.println("Application inits");
+    }
+
+    public void stop() throws Exception {
+        System.out.println("Application stops");
+    }
 }
